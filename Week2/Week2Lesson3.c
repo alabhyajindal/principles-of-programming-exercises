1ebbf43554612456f4084eab04645831a04f9601
Alabhya Jindal <alabhya10@gmail.com>
2024-10-08 10:49:23 +0100

    week 2 3

--- Week2/Week2Lesson3.c
+++ Week2/Week2Lesson3.c
@@ -2,15 +2,33 @@
 #include <stdbool.h>
 
 /*
- * Write a function which returns true if a number is divisible by 3 and  
+ * Write a function which returns true if a number is divisible by 3 and
  * false if it is not.  Hint: use the modulo (%) operator.
  *
- * Modify your code so that your function now returns true if a number is divisible 
+ * Modify your code so that your function now returns true if a number is divisible
  * by 3 or 5 and false if it is not.
  *
-*/
+ */
 
-int main(void) {
-  printf("Hello World\n");
+bool check_div(int n)
+{
+  if (n % 3 == 0 || n % 5 == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+int main(void)
+{
+  bool check27 = check_div(27);
+  printf("%d\n", check27);
+  bool check20 = check_div(20);
+  printf("%d\n", check20);
+  bool check73 = check_div(73);
+  printf("%d\n", check73);
   return 0;
 }
