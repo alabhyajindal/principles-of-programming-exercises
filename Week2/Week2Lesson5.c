7221e516cfc988c70b9be90e1a8b7c105d6f359d
Alabhya Jindal <alabhya10@gmail.com>
2024-10-08 11:32:46 +0100

    week 2 5

--- Week2/Week2Lesson5.c
+++ Week2/Week2Lesson5.c
@@ -9,11 +9,28 @@
  * For example if we call fibonacci(1) we should get the value 1 returned,
  * if we call fibonacci(4) we should get the value 3 returned.
  */
-int fibonacci(int term) {
-  
+long int fibonacci(long int term)
+{
+  if (term == 1 || term == 2)
+  {
+    return 1;
+  }
+  long int prev = 1;
+  long int next = 1;
+  for (long int i = 3; i <= term; i++)
+  {
+    long int temp = next;
+    next = prev + next;
+    prev = temp;
+  }
+  return next;
 }
 
-int main(void) {
-  printf("Hello World\n");
+long int main(void)
+{
+  long int test_1 = fibonacci(4);
+  printf("%ld\n", test_1);
+  long int test_2 = fibonacci(91);
+  printf("%ld\n", test_2);
   return 0;
 }
