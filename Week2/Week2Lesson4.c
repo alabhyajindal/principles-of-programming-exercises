c7cba8a4238629e5c5c4f73f65d7d2d484b119ba
Alabhya Jindal <alabhya10@gmail.com>
2024-10-08 11:06:28 +0100

    week 2 4

--- Week2/Week2Lesson4.c
+++ Week2/Week2Lesson4.c
@@ -3,11 +3,32 @@
  * Complete the recursive function below which performs multiplication of two
  * numbers without using the inbuilt * sign
  */
-int multiply(int number, int by) {
-  
+int multiply_iterative(int number, int by)
+{
+  int result = 0;
+  for (int i = 0; i < by; i++)
+  {
+    result += number;
+  }
+  return result;
 }
 
-int main(void) {
-  printf("The multiplication of 2 by 3 is %d\n", multiply(2, 3));
+int multiply_recursive(int number, int by)
+{
+  if (by == 1)
+  {
+    return number;
+  }
+  else
+  {
+    return number + multiply_recursive(number, by - 1);
+  }
+}
+
+int main(void)
+{
+  printf("The multiplication of 2 by 3 is %d\n", multiply_iterative(2, 3));
+  printf("The multiplication of 2 by 3 is %d\n", multiply_recursive(2, 3));
+  printf("The multiplication of 10 by 5 is %d\n", multiply_recursive(10, 5));
   return 0;
 }
