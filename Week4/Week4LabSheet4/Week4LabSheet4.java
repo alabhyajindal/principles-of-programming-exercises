4a88d8a7ee092d08a2aa314ec04ec09182507b30
Alabhya Jindal <alabhya10@gmail.com>
2024-11-22 15:32:15 +0000

    typo

--- Week4/Week4LabSheet4/Week4LabSheet4.java
+++ Week4/Week4LabSheet4/Week4LabSheet4.java
@@ -9,7 +9,7 @@
  In the Week4LabSheet4 class (i.e. this class) create two different BankAccount instances from its main method (i.e. two BankAccount objects). Demonstrate the use of your mutators and accessors within that same main method.
 */
 
-publi class Week4LabSheet4 {
+public class Week4LabSheet4 {
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
